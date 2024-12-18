61934e66ad9a3e777152a2997c52f23e9243e352
Abhishek <abhishekmaddheshiya632@gmail.com>
2024-12-18 13:40:26 +0530

    add undo and redo feature in counter

--- src/components/Basic_Project/Counter.jsx
+++ src/components/Basic_Project/Counter.jsx
@@ -3,21 +3,50 @@ import React, { useState } from 'react'
 const Counter = () => {
 
     const [count, setCount] = useState(0);
-    
+    const [historyCount, setHistoryCount] = useState([0]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
     const incrementCount = () => {
-        setCount(count+1);
+       const newCount = count + 1;
+       const updatedHistory = historyCount.slice(0, currentIndex+ 1);
+       setHistoryCount([...updatedHistory, newCount]);
+       setCurrentIndex(currentIndex + 1);
+       setCount(newCount); 
     }
 
     const decrementCount = () => {
-        setCount(count-1);
+        const newCount = count - 1;
+       const updatedHistory = historyCount.slice(0, currentIndex+ 1);
+       setHistoryCount([...updatedHistory, newCount]);
+       setCurrentIndex(currentIndex + 1);
+       setCount(newCount);
     }
+
+    const undoCount = () => {
+        if (currentIndex > 0){
+            const newIndex = currentIndex - 1;
+            setCurrentIndex(newIndex);
+            setCount(historyCount[newIndex]);
+        }
+    }
+
+    const redoCount = () => {
+        if (currentIndex < historyCount.length -1){
+            const newIndex = currentIndex + 1;
+            setCurrentIndex(newIndex);
+            setCount(historyCount[newIndex]);
+        }
+    }
+
 
     return (
         <div>
             <h1 className='text-black text-5xl font-semibold text-center mb-5'>Count : {count}</h1>
             <div className='space-x-3'>
-                <button onClick={incrementCount} className='px-5 py-2 rounded bg-green-600 text-white border-none'>Increment</button>
-                <button onClick={decrementCount} className='px-5 py-2 rounded bg-indigo-600 text-white border-none'>Decrement</button>
+                <button onClick={incrementCount} className='w-32 px-5 py-2 rounded bg-green-600 text-white border-none'>Increment</button>
+                <button onClick={decrementCount} className='w-32 px-5 py-2 rounded bg-indigo-600 text-white border-none'>Decrement</button>
+                <button onClick={undoCount} className='w-32 px-5 py-2 rounded bg-teal-600 text-white border-none'>Undo</button>
+                <button onClick={redoCount} className='w-32 px-5 py-2 rounded bg-gray-600 text-white border-none'>Redo</button>
             </div>
         </div>
     )
